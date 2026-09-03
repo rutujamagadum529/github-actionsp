cc1c78badc29a22f8722a04b6d89b9c3a9996de3
rutujamagadum529 <114569087+rutujamagadum529@users.noreply.github.com>
2026-09-03 17:41:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 from flask import Flask, render_template
+
+
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return render_template('index.html')
+@app.route("/")
+def home():
+    return render_template("index.html")
 
 
-@app.route('/health')
-def health():
-    return 'Server is up and running'
+@app.route("/hello")
+def hello():
+    return "Hello Dosto! This is my Flask Application."
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
